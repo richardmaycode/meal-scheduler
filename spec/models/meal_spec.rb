9c0da304bb66e0619c6559b8b7e7d526622e019c
Richard Wise <orsusbass@gmail.com>
2020-05-18 15:50:52 -0500

    fix meal spec and create pages#home

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Meal, type: :model do
-  subject {create(:meal)}
+  subject { create(:meal) } 
   context 'associations' do
     it { should define_enum_for(:status) }
   end
@@ -11,7 +11,7 @@ RSpec.describe Meal, type: :model do
     end
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
-    it { should validate_length_of(:name).is_at_least(5).is_at_most(15) }
+    it { should validate_length_of(:name).is_at_least(5).is_at_most(75) }
     it { should validate_presence_of(:status) }
   end
 end
