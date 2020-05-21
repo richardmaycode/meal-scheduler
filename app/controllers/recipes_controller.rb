dfa9d0a41cdee36ec822590b661d4c6831808f8a
Richard Wise <orsusbass@gmail.com>
2020-05-20 20:20:22 -0500

    create recipes controller

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,43 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: %w[show edit update delete]
+  
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show; end
+
+  def new; end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to recipes_path
+    else
+      render :edit
+    end
+  end
+
+  def delete
+  end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit()
+    end
+
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/create'
+  get 'recipes/edit'
+  get 'recipes/update'
+  get 'recipes/delete'
   get 'pages/home'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
